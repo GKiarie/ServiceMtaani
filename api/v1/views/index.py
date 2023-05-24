c74e621bc67fe0734934d50ec36debc0431e2527
GKiarie <igitaukiarie@gmail.com>
2023-05-24 13:44:51 +0300

    api set up

---
+++ api/v1/views/index.py
@@ -0,0 +1,8 @@
+#!/usr/bin/pyhton3
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """return api status"""
+    return jsonify({"status": "OK"})
