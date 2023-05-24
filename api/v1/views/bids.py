3005c769962229b1864be2297ca602fbeee1d8d5
GKiarie <igitaukiarie@gmail.com>
2023-05-24 22:42:47 +0300

    updated api endpoints

--- api/v1/views/bids.py
+++ api/v1/views/bids.py
@@ -33,27 +33,27 @@ def get_bids_job_id(job_id):
             obj.save()
             return jsonify(obj.to_dict()), 201
 
-@app_views.route('/jobs/<job_id>/bids/<bid_id>', strict_slashes=False,
-                 methods=["GET", "PUT", "DELETE"])
-def get_bids_job_id(job_id, bid_id):
-    job_objs = storage.get(Job, job_id)
-    bid_objs = storage.get(Bid, bid_id)    
-    if job_objs is None or bid_objs is None:
-        abort(404, "Invalid job or bid id")
+# @app_views.route('/jobs/<job_id>/bids/<bid_id>', strict_slashes=False,
+#                  methods=["GET", "PUT", "DELETE"])
+# def get_bids_job_id(job_id, bid_id):
+#     job_objs = storage.get(Job, job_id)
+#     bid_objs = storage.get(Bid, bid_id)    
+#     if job_objs is None or bid_objs is None:
+#         abort(404, "Invalid job or bid id")
 
-    if request.method == "GET":
-        bid_objs = storage.all(Bid).values()
-        list_dict = [obj.to_dict() for obj in bid_objs if obj.job_id == job_id]
-        return jsonify(list_dict), 200
+#     if request.method == "GET":
+#         bid_objs = storage.all(Bid).values()
+#         list_dict = [obj.to_dict() for obj in bid_objs if obj.job_id == job_id]
+#         return jsonify(list_dict), 200
     
-    elif request.method == "POST":
-        my_dict = request.get_json()
-        if not my_dict:
-            abort(400, "Not a JSON")
-        if not my_dict.get('mechanic_id'):
-            abort(400, "Missing mechanic id")
-        else:
-            my_dict["job_id"] = job_id
-            obj = Bid(**my_dict)
-            obj.save()
-            return jsonify(obj.to_dict()), 201
+#     elif request.method == "POST":
+#         my_dict = request.get_json()
+#         if not my_dict:
+#             abort(400, "Not a JSON")
+#         if not my_dict.get('mechanic_id'):
+#             abort(400, "Missing mechanic id")
+#         else:
+#             my_dict["job_id"] = job_id
+#             obj = Bid(**my_dict)
+#             obj.save()
+#             return jsonify(obj.to_dict()), 201
