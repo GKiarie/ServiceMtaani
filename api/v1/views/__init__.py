31b139d2145f9b03996d1974c683e97d154665dc
Mdigo12 <mdigojacob@gmail.com>
2023-05-25 12:01:38 +0300

    import client, vendors

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,3 +6,6 @@ from api.v1.views.index import *
 from api.v1.views.jobs import *
 from api.v1.views.bids import *
 from api.v1.views.mechanics import *
+from api.v1.views.clients import *
+from api.v1.views.vendors import *
+from api.v1.views.reviews import *
