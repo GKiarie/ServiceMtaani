51f4e83f0b7005bfc986944f5030a4f36a9f916e
Mdigo12 <mdigojacob@gmail.com>
2023-05-31 11:18:39 +0300

    dbstorage

--- models/engine/dbstorage.py
+++ models/engine/dbstorage.py
@@ -100,7 +100,7 @@ class DBStorage:
         total items in db if no class is passed"""
         obj_dict = self.all(cls)
         return len(obj_dict)
-    
+
     def openjobs(self):
         """Return a list of open jobs"""
         jobs = self.__session.query(Job).filter(Job.status == True).all()
